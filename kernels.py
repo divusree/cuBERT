3113fa7514191f42407cb0f32ae376c2718137c5
divusree <divusree@gmail.com>
2025-04-10 19:38:36 +0530

    fa2 backward

--- kernels.py
+++ kernels.py
@@ -353,6 +353,116 @@ def fa_fwd(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor):
 
 
 @triton.jit
+def fa_bwd_inner_kernel(q_ptr, k_ptr, v_ptr,
+                        o_ptr, dO_ptr, L_ptr, qk_scale,
+                        dQ_ptr, dK_ptr, dV_ptr, D_ptr, 
+                        M: tl.constexpr , N: tl.constexpr, 
+                        stride_qm, stride_qn, # all of the matrices have the same stride
+                        stride_lm, stride_ln, 
+                        BLOCK_SIZE_M: tl.constexpr, 
+                        BLOCK_SIZE_N: tl.constexpr 
+                        ):
+
+
+    pid_row = tl.program_id(axis = 0)
+    pid_col = tl.program_id(axis = 1)
+
+ 
+    offset_kv_row_block = (pid_row * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offset_kv_col_block = (pid_col * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % BLOCK_SIZE_N # check the modulo
+
+    offset = offset_kv_row_block[:,None] * stride_qm + offset_kv_col_block[None,:] *stride_qn
+    v_ptr += offset
+    k_ptr += offset
+    kv_mask = (offset_kv_row_block[:,None] < M) & (offset_kv_col_block[None, :] < N)
+    K_j = tl.load(k_ptr, mask = kv_mask, other = 0)
+    V_j = tl.load(v_ptr, mask = kv_mask, other = 0)
+
+    offset_row_block =  tl.arange(0, BLOCK_SIZE_M)% M
+    offset_col_block = tl.arange(0, BLOCK_SIZE_N) % BLOCK_SIZE_N # check the modulo
+
+    q_ptr += offset_row_block[:,None] * stride_qm + offset_col_block[None,:] *stride_qn
+    o_ptr += offset_row_block[:,None] * stride_qm + offset_col_block[None,:] *stride_qn
+    dO_ptr += offset_row_block[:,None] * stride_qm + offset_col_block[None,:] *stride_qn
+    dQ_ptr += offset_row_block[:,None] * stride_qm + offset_col_block[None,:] *stride_qn
+    q_mask  = (offset_row_block[:,None] < M) & (offset_col_block[None, :] < N)
+    dQ_offset = offset_row_block[:,None] * stride_qm + offset_col_block[None,:] *stride_qn
+
+    L_ptr += offset_row_block[:,None] * stride_lm #+offset_col_block[None,:] *stride_ln
+    D_ptr += offset_row_block[:,None] * stride_lm #+offset_col_block[None,:] *stride_ln
+    L_mask =(offset_row_block[:,None] < M) #and (offset_col_block[None,:] < N) 
+
+    dV_j = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype = tl.float32)
+    dK_j = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype = tl.float32)
+
+    for i in range(0, tl.cdiv(M, BLOCK_SIZE_M)):
+
+        
+        Q_i = tl.load(q_ptr, mask = q_mask, other = 0) # check mask
+        # O_i = tl.load(o_ptr, mask = q_mask, other = 0) # check mask
+        dO_i = tl.load(dO_ptr, mask = q_mask, other = 0) # check mask
+        dQ_i = tl.load(dQ_ptr, mask = q_mask, other = 0) # check mask
+        L_i = tl.load(L_ptr, mask = L_mask, other = 0) 
+        D_i = tl.load(D_ptr, mask = L_mask, other = 0) 
+
+        # back pass
+        S_i = tl.dot(Q_i, K_j.trans(1,0)) * qk_scale
+        P_i = tl.exp(S_i - L_i)
+        # if ((pid_row == 0) and (pid_col == 0)) and (i == 0):
+        #     tl.device_print("S_i - L_i", S_i - L_i)
+        dV_j += tl.dot(P_i.T, dO_i)
+        dP_i = tl.dot(dO_i, V_j.T)
+        dS_i = P_i * (dP_i - D_i)
+        dQ_i += tl.dot(dS_i, K_j) # write back to HBM
+        dK_j += tl.dot(dS_i.T, Q_i)
+
+        tl.store(dQ_ptr + dQ_offset, dQ_i, mask = q_mask)
+
+        q_ptr += BLOCK_SIZE_M * stride_qm
+        dO_ptr += BLOCK_SIZE_M * stride_qm
+        dQ_ptr += BLOCK_SIZE_M * stride_qm
+        L_ptr += BLOCK_SIZE_M * stride_lm
+        D_ptr += BLOCK_SIZE_M * stride_lm
+
+    tl.store(dK_ptr + offset, dK_j, mask = kv_mask)
+    tl.store(dV_ptr + offset, dV_j, mask = kv_mask)
+
+
+def fa_bwd(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor,
+                 O: torch.Tensor, dO: torch.Tensor,
+                 L: torch.Tensor):
+    """
+    inputs - dO from torch - upstream gradient 
+
+    outputs - dQ, dK, dV - downstream gradient. helper vars - dS, dP
+    """
+    # B, M, N = Q.shape
+    assert Q.is_cuda and K.is_cuda and V.is_cuda and O.is_cuda and dO.is_cuda and L.is_cuda
+
+    M, N = Q.shape
+    qk_scale = 1/ math.sqrt(N)
+
+    dQ = torch.zeros_like(Q, device = 'cuda', dtype = torch.float32)
+    dK = torch.zeros_like(K, device = 'cuda', dtype = torch.float32)
+    dV = torch.zeros_like(V, device = 'cuda', dtype = torch.float32)
+
+    D = O * dO # optimize?
+    D = D.sum(dim = 1).reshape(M, 1)
+
+    grid = lambda meta: (triton.cdiv(M, meta['BLOCK_SIZE_M']), 1)
+    fa_bwd_inner_kernel[grid](
+                            Q, K, V, 
+                            O, dO, L, qk_scale,
+                            dQ, dK, dV, D,
+                            M, N,      
+                            Q.stride(0), Q.stride(1), 
+                            L.stride(0), L.stride(1), 
+                            BLOCK_SIZE_M = 16,
+                            BLOCK_SIZE_N = triton.next_power_of_2(N))   
+  
+    return dQ, dK, dV
+
+@triton.jit
 def softmax_kernel(
     x_ptr,
     output_ptr,
